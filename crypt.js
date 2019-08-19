4b316e332999f283ea425baa0dfee5d844c6352e
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-08-19 16:38:59 +0100

    encrypted with undefined

--- crypt.js
+++ crypt.js
@@ -24,10 +24,22 @@ while(copyStr.length > 0) {
     result.push(copyStr.splice(0, recLength));
 }; // this creates sub arrays in the main array. Their number is the breadth
 
+var codeArr = [];
+for (var i = 0; i < result.length; i++) {
+    for (var j = 0; j < result.length; j++) {
+        //console.log(result[i][j]);
+        //console.log(result[i][j].slice(0));
+        //if (codeArr.length<result.length) {
+            codeArr += result[j][i];
+            //alert(codeArr);         
+        };
+    }
+//};
 
+console.log(codeArr);
 // this joins each sub array to make it a string 
 ans = "<p>";
-result.forEach(function(value){
+result.forEach(value => {
     ans +="<span>" + "\"" + value.join("") + "\"" + "</span>";
     ans += "</p>"
     document.getElementById("test").innerHTML = ans;
@@ -36,4 +48,4 @@ result.forEach(function(value){
 console.log(result[0][1] + result[1][1] + result[2][1] + 
     result[3][1] + result[4][1] + result[5][1] + result[6][1] );
 
-
+document.getElementById("run").innerHTML= "<p>" + codeArr + "</p>";
